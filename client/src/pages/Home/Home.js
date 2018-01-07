87b256864db2b6e6015d1b5e46798e68c4ec46eb
Corey Rodems <coreyrodems@gmail.com>
2018-01-06 23:15:42 -0800

    removing aos

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -3,7 +3,6 @@ import Carousel from '../../components/Carousel'
 import About from '../../components/About'
 import Contact from '../../components/Contact'
 import Header from '../../components/Header'
-import ScrollAnimation from 'react-animate-on-scroll';
 import './Home.css'
 
 import axios from 'axios'
@@ -18,24 +17,18 @@ class Home extends Component {
     render(){
         return(
             <div className='container-fluid'>            
-              
                 <div className='row container mx-auto'>
                     <div className='col-md-12'>
-                        <ScrollAnimation offset={725} animateIn='fadeIn'animateOut='fadeOut'>
                         <About />
-                        </ScrollAnimation>
                     </div>
                 </div>
                 <div className='seperator mx-auto'>
                 </div>
                 <div className='row container mx-auto'>
                     <div className='col-md-12 '>
-                        <ScrollAnimation offset={600} animateIn='fadeIn'animateOut='fadeOut'>
                         <Contact />
-                        </ScrollAnimation>
                     </div>
                 </div>
-                
             </div>
         )
     }
